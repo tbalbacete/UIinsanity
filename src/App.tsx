18b9439a9f268c1c4c173c5b4949fd6b09dbea85
Tony Albacete <tbalbacete@gmail.com>
2023-09-29 06:57:43 -0400

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
-import { Box, Title } from '@mantine/core'
+import { Box, Button, TextInput } from '@mantine/core'
 
 export const App: React.FC = () => {
 
   return (
-    <Box>
-      <Title>
-        Hello World
-      </Title>
+    <Box bg="#FFFFFF" c="#000" style={{ padding: "2rem 5rem", border: "1px solid grey", borderRadius: "8px" }}>
+      <TextInput bg="#FFFFFF" label="Username" placeholder="yourusername@website.com" />
+      <TextInput bg="#FFFFFF" label="Password" placeholder="" />
+      <Box>
+        <Button size="s">Submit</Button>
+      </Box>
+
     </Box>
   )
 }
